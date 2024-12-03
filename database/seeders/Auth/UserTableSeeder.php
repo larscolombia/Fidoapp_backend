7d383fe971f50670d412f869252be487f45210b1
Jaime Antonio Lugo Torrealba <lugotorrealba@gmail.com>
2024-12-03 12:54:26 -0400

    fix:removiendo pawlly de los seeders

--- database/seeders/Auth/UserTableSeeder.php
+++ database/seeders/Auth/UserTableSeeder.php
@@ -27,13 +27,13 @@ class UserTableSeeder extends Seeder
 
         // Add the master administrator, user id of 1
         $avatarPath = config('app.avatar_base_path');
-       
+
 
         $users = [
             [
               'first_name' => 'Super',
               'last_name' => 'Admin',
-              'email' => 'admin@pawlly.com',
+              'email' => 'admin@fidoapp.com',
               'password' => Hash::make('12345678'),
               'mobile' => '44-5289568745',
               'date_of_birth' => fake()->date,
@@ -45,7 +45,7 @@ class UserTableSeeder extends Seeder
               'updated_at' => Carbon::now(),
               'user_type' => 'admin',
             ],
-            
+
             [
               'first_name' => 'John',
               'last_name' => 'Doe',
@@ -238,10 +238,10 @@ class UserTableSeeder extends Seeder
             //   'updated_at' => Carbon::now(),
             //   'user_type' => 'demo_admin',
             // ]
-            
-           
+
+
         ];
-        
+
         if (env('IS_DUMMY_DATA')) {
           foreach ($users as $key => $user_data) {
               $featureImage = $user_data['profile_image'] ?? null;
@@ -250,16 +250,16 @@ class UserTableSeeder extends Seeder
 
               if (isset($user_data['address'])) {
                 $addresses = $user_data['address'];
-                
+
                 foreach($addresses as $addressData){
                     $address = new Address($addressData);
                     $user->address()->save($address);
                 }
               }
-        
+
               $user->assignRole($user_data['user_type']);
-            
-         
+
+
               event(new UserCreated($user));
               if (isset($featureImage)) {
                   $this->attachFeatureImage($user, $featureImage);
