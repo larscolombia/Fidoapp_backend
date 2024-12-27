0c40fec1bddc889d6d9fba8d3fd22760532e6c83
Jaime Antonio Lugo Torrealba <lugotorrealba@gmail.com>
2024-12-27 13:36:49 -0400

    fix:agregado la compra de curso en base a los fidocoins del uusuario y con notificacion

--- lang/es/course_platform.php
+++ lang/es/course_platform.php
@@ -33,4 +33,5 @@ return [
     'add_video' => 'Agregar video',
     'thumbnail' => 'Miniatura',
     'Are you sure you want to delete this course platform?' => '¿Estás seguro de que quieres eliminar este curso de la plataforma?',
+    'buy_course' => 'Compra del curso ',
 ];
