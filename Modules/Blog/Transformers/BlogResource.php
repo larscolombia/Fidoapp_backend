f057fb39ad44778bd015628e28f5f5d149da7f42
Jaime Antonio Lugo Torrealba <lugotorrealba@gmail.com>
2025-02-07 12:53:27 -0400

    fix

--- Modules/Blog/Transformers/BlogResource.php
+++ Modules/Blog/Transformers/BlogResource.php
@@ -27,9 +27,9 @@ class BlogResource extends JsonResource
             'created_by' => $this->created_by,
             'updated_by' => $this->updated_by,
             'deleted_by' => $this->deleted_by,
-            'created_at' => !is_null($this->created_at) ? Carbon::parse($this->created_at)->format('d-m-Y') : null,
-            'updated_at' => !is_null($this->updated_at) ? Carbon::parse($this->updated_at)->format('d-m-Y') : null,
-            'deleted_at' => !is_null($this->deleted_at) ? Carbon::parse($this->deleted_at)->format('d-m-Y') : null,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at ,
+            'deleted_at' => $this->deleted_at,
         ];
     }
 }
